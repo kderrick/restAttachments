b683d4af1bf86ddecbc7a628415fde63ebb04bee
Kyle <kderrick1988@gmail.com>
2018-12-13 15:56:19 -0800

    add upload method to create the attachment upload endpoint

--- src/main/java/com/kyle/restws/FileService.java
+++ src/main/java/com/kyle/restws/FileService.java
@@ -6,10 +6,26 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
+
+@Path("/fileService")
 public class FileService {
 
-	private static final String FILE_PATH = "";
+	private static final String FILE_PATH = "/Users/kylederrick/Documents/workspace";
+
+	@Path("/upload")
+	@POST
+	public void upload(List<Attachment> attachments) throws FileNotFoundException, IOException {
+		for (Attachment attachment : attachments) {
+			copyFile(attachment.getDataHandler().getInputStream());
+			
+		}
+	}
 
 	private void copyFile(InputStream inputStream) throws FileNotFoundException, IOException {
 		OutputStream out = null;
